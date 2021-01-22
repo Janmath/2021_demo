47aa02d5b7a94fc410e0fe5c9649aa2f2436ff87
wanghuawei <wanghuawei@gongchang.com>
2021-01-22 14:37:04 +0800

    add

--- src/Demo/Test.php
+++ src/Demo/Test.php
@@ -3,8 +3,13 @@ namespace Demo;
 
 class Test
 {
-	public static function getTomorrowTime()
-	{
-		return time()+86400;
-	}
+    public static function getTomorrowTime()
+    {
+        return time() + 86400;
+    }
+
+    public static function hello()
+    {
+        return 'Hello Body';
+    }
 }
